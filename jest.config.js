2781a0411c2e6c939f82e30517ebfdea351c6f6e
MaxLiebsch <liebsch@dipmaxexport.com>
2025-05-16 12:15:54 +0200

    Fix testPathIgnorePatterns to use forward slashes
    Updates the regex patterns in Jest configuration to use forward slashes
    instead of escaped backslashes. This ensures path pattern matching
    works consistently across different operating systems.

--- jest.config.js
+++ jest.config.js
@@ -161,9 +161,9 @@ const config = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-    "\\\\node_modules\\\\",
-    "\\\\tests\\\\*",
-    "\\\\test-examples\\\\*",
+    "/node_modules/",
+    "/tests/*",
+    "/test-examples/*",
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
